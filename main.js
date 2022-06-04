2b4455e5fdc1d2b7978dda3e44150ea19bb7baa3
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-06-04 13:55:22 -0500

    reto del boton

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const URL = 'https://api.thedogapi.com/v1/images/search';
 // const URLmaterial = 'https://api.mpds.io/v0/download/p?q=P50042118-3&fmt=json&sid=null&ed=0';
 
-fetch(URL)
+/* fetch(URL)
     .then(res => res.json())
     .then(data => {
         const img = document.querySelector('img');
         img.src = data[0].url;
-    });
+    }); */
+
+const asyncFunction = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+}
+
+const boton = document.querySelector('button');
+asyncFunction();
+boton.onclick = asyncFunction;
