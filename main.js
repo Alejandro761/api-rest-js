5cd66e7242af4a4dae2a0ebc402c5d5f188397d6
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-06-07 17:54:42 -0500

    borrando perritos con delete y header de autenticacion. 12 y 14

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=28395ef1-601a-475d-9561-5532a98eacbc'; //limit es el atributo que nos indica el limite de imagenes, para ponerlo debes poner antes un '?'
-const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?api_key=28395ef1-601a-475d-9561-5532a98eacbc';
-
+const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2'; //limit es el atributo que nos indica el limite de imagenes, para ponerlo debes poner antes un '?'
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites';
+const API_URL_FAVORITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}`;
 const spanError = document.getElementById('error');
 
 // const URLmaterial = 'https://api.mpds.io/v0/download/p?q=P50042118-3&fmt=json&sid=null&ed=0';
@@ -26,7 +26,9 @@ const loadRandomDogs = async () => {
         const btn2 = document.getElementById('btn2');
 
         img1.src = data[0].url;
+        img1.width = 150
         img2.src = data[1].url;
+        img2.width = 150
 
         btn1.onclick = () => saveFavouriteDog(data[0].id);
         btn2.onclick = () => saveFavouriteDog(data[1].id);
@@ -35,15 +37,26 @@ const loadRandomDogs = async () => {
 }
 
 const loadFavouritesDogs = async () => {
-    const response = await fetch(API_URL_FAVORITES);
+    const response = await fetch(API_URL_FAVORITES, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': '28395ef1-601a-475d-9561-5532a98eacbc'
+        }
+    });
     const data = await response.json();
     console.log(data);
     
     if(response.status !== 200){
         spanError.innerHTML = 'Hubo un error: ' + response.status + data.message; 
     } else {
+        const section = document.getElementById('favoriteDogs')
+        section.innerHTML = '';
+        const h2 = document.createElement('h2');
+        const h2Text = document.createTextNode('Perritos favoritos');
+        h2.appendChild(h2Text);
+        section.appendChild(h2);
+
         data.forEach(perrito => {
-            const section = document.getElementById('favoriteDogs')
             const article = document.createElement('article');
             const img = document.createElement('img');
             const btn = document.createElement('button');            
@@ -52,6 +65,7 @@ const loadFavouritesDogs = async () => {
             img.src = perrito.image.url;
             img.width = 150;
             btn.appendChild(btnText); //append child hace que btnText sea su hijo 
+            btn.onclick = () => deleteFavouriteDog(perrito.id);
             article.appendChild(img);
             article.appendChild(btn);
             section.appendChild(article);
@@ -63,7 +77,8 @@ const saveFavouriteDog = async (id) => {
     const response = await fetch(API_URL_FAVORITES, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-API-KEY': '28395ef1-601a-475d-9561-5532a98eacbc'
         },
         body: JSON.stringify({
             image_id: id
@@ -75,6 +90,28 @@ const saveFavouriteDog = async (id) => {
 
     if(response.status !== 200){
         spanError.innerHTML = 'Hubo un error: ' + response.status + data.message; 
+    } else {
+        console.log('Guardado.');
+        loadFavouritesDogs();
+    }
+}
+
+const deleteFavouriteDog = async (id) => {
+    const response = await fetch(API_URL_FAVORITES_DELETE(id), {
+        method: 'DELETE',
+        headers: {
+            'X-API-KEY': '28395ef1-601a-475d-9561-5532a98eacbc'
+        }
+    });
+
+    const data = await response.json();
+    console.log(response);
+
+    if(response.status !== 200){
+        spanError.innerHTML = 'Hubo un error: ' + response.status + data.message; 
+    } else {
+        console.log('Eliminado.');
+        loadFavouritesDogs();
     }
 }
 
