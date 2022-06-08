017e55cf505a6fe2e6a794876e66cbba05048c97
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-06-07 22:46:50 -0500

    axios. 17.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const api = axios.create({
+    baseURL: 'https://api.thedogapi.com/v1/',
+    //si ocupamos axios, baseURL guardara la url principal para no estar
+    //cambiandola dependiendo del metodo http
+});
+api.defaults.headers.common['X-API-KEY'] = '28395ef1-601a-475d-9561-5532a98eacbc'; //el header del metodo ya se guarda aqui, asi q si usamos axios ya no hara falta llamarlo
+
 const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2'; //limit es el atributo que nos indica el limite de imagenes, para ponerlo debes poner antes un '?'
 const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites';
 const API_URL_FAVORITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}`;
@@ -75,22 +82,25 @@ const loadFavouritesDogs = async () => {
 }
 
 const saveFavouriteDog = async (id) => {
-    const response = await fetch(API_URL_FAVORITES, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-API-KEY': '28395ef1-601a-475d-9561-5532a98eacbc'
-        },
-        body: JSON.stringify({
-            image_id: id
-        }),
-    });
+    const {data, status} = await api.post('favourites', {
+        image_id: id, //no hace falta ocupar JSON.stringify
+    }); //solo agregamos lo que le falta (favourites) a la url porque ya esta guardado en la linea 1
+    // const response = await fetch(API_URL_FAVORITES, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'X-API-KEY': '28395ef1-601a-475d-9561-5532a98eacbc'
+    //     },
+    //     body: JSON.stringify({
+    //         image_id: id
+    //     }),
+    // });
 
-    const data = await response.json();
-    console.log(response);
+    // const data = await response.json();
+    // console.log(response);
 
-    if(response.status !== 200){
-        spanError.innerHTML = 'Hubo un error: ' + response.status + data.message; 
+    if(status !== 200){
+        spanError.innerHTML = 'Hubo un error: ' + status + data.message; 
     } else {
         console.log('Guardado.');
         loadFavouritesDogs();
